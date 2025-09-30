97be0915ea9ab89c7c1f258862f7cf667e66c868
josemanuelcruz22 <jose.cruz.martinez.al@iespoligonosur.org>
2025-09-30 16:43:31 +0000

    ejercicio 3 hecho

---
+++ ej03.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ej03 {
+    public static void main(String[] args)
+   
+ {
+      System.out.println("Escribe tu edad");
+      Scanner sc = new Scanner(System.in);
+      int edad = sc.nextInt();
+      System.out.println("tienes " + edad + " años ");
+    }
+}
