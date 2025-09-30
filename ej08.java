46498852f997cc53b9db12251dcd57232c36b672
josemanuelcruz22 <jose.cruz.martinez.al@iespoligonosur.org>
2025-09-30 17:05:25 +0000

    ejercicio 8 hecho

---
+++ ej08.java
@@ -0,0 +1,12 @@
+public class ej08 {
+    public static void main(String[] args) {
+      int edad = 22;
+      double dinero = 2.22;
+      char miLetra = 'A';
+      boolean correcto = true;
+      System.out.println(edad);
+      System.out.println(dinero);
+      System.out.println(miLetra);
+      System.out.println(correcto);
+    }
+}
