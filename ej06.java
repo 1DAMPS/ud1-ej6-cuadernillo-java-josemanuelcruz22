fe9a695853e36d0e6d2344514acb27c778556d9b
josemanuelcruz22 <jose.cruz.martinez.al@iespoligonosur.org>
2025-09-30 16:51:08 +0000

    subida ejercicio6

---
+++ ej06.java
@@ -0,0 +1,10 @@
+public class ej06 {
+    public static void main(String[] args) {
+        int numeroPrimero = 1;
+        int numeroSegundo = 2;
+        int numeroTercero = 3;
+         System.out.println(numeroPrimero);
+         System.out.println(numeroSegundo);
+         System.out.println(numeroTercero);
+    }
+}
