39a2e7115cbd85bfd511bec3658c7a3b348cc63f
josemanuelcruz22 <jose.cruz.martinez.al@iespoligonosur.org>
2025-09-30 16:32:47 +0000

    ejercicio 2 terminado

---
+++ ej02.java
@@ -0,0 +1,8 @@
+public class ej02 {
+    public static void main(String[] args) {
+        String nombre = "Jose Manuel";
+        int edad = 23;
+        System.out.println(nombre);
+        System.out.println(edad);
+    }
+}
