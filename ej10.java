b01bff403e67367aa6ade5fb6fd9c38cc7fd27c9
josemanuelcruz22 <jose.cruz.martinez.al@iespoligonosur.org>
2025-10-01 14:15:50 +0000

    ejercicio 10 hecho

---
+++ ej10.java
@@ -0,0 +1,8 @@
+public class ej10 {
+    public static void main(String[] args) {
+      short numero = 32000;
+      byte numero2 = 22;
+      System.out.println(numero);
+      System.out.println(numero2);
+    }
+}
