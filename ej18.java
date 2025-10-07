696d2297c0451444d5cf714524f34f2f55a68c60
josemanuelcruz22 <jose.cruz.martinez.al@iespoligonosur.org>
2025-10-07 19:14:59 +0200

    Adjunto ejercicio 18 de comentarios

---
+++ ej18.java
@@ -0,0 +1,17 @@
+/**
+ * Nombre: Jose Manuel Cruz
+ * Fecha: 07/10/2025
+ * Propósito: Ejemplo de programa con comentarios en Java
+ */
+public class ej18 {
+    public static void main(String[] args) {
+        // comentario una linea
+        int numero = 10;
+        /*
+         Comentario multilínea:
+        Esto es un comentario multilínea
+        Creado por jose manuel
+        */
+     System.out.println("El número es: " + numero);
+     }
+}
