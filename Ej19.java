201fdef025448ffd1ed29d252c2bde6b24fc2e8c
josemanuelcruz22 <jose.cruz.martinez.al@iespoligonosur.org>
2025-10-14 19:10:49 +0200

    Ejercicios faltantes

---
+++ Ej19.java
@@ -0,0 +1,10 @@
+public class Ej19 {
+   public static void main (String[] args) {
+
+    int radio = 5; // aqui declarariamos una variable y la inicializamos con el valor 5
+    double area = 3.1416 * radio * radio; // aqui creariamos una formula para tener el area de un circulo, aunque yo mejoraria el codigo creando una constante a PI
+    System.out.println(area); // Aqui mostrariamos el area del circulo
+
+   }
+}
+ 
---
+++ Ej20.java
@@ -0,0 +1,13 @@
+/**
+ * Programa que muestra el resultado de una multiplicación.
+ * @author Jose Manuel Cruz
+ * @version 2.0
+ */
+public class Ej20 {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 5;
+        int producto = a * b;
+        System.out.println("El resultado es: " + producto);
+    }
+}
